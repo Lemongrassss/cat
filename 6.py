d14aa4b4a6ef47afc85205c7bd05d3547902a674
Lemongrassss <i.pineappleee3@gmail.com>
2025-01-13 19:29:39 +0300

    Add files via upload

---
+++ 6.py
@@ -0,0 +1,44 @@
+class Vehicle:
+    def __init__(self, make, model):
+        self.make = make  # Марка
+        self.model = model  # Модель
+
+    def get_info(self):
+        return f"Марка: {self.make}, Модель: {self.model}"
+
+class Car(Vehicle):
+    def __init__(self, make, model, fuel_type):
+        super().__init__(make, model)  # Инициализация атрибутов базового класса
+        self.fuel_type = fuel_type  # Тип топлива
+
+    def get_info(self):
+        base_info = super().get_info()  # Получение информации из базового класса
+        return f"{base_info}, Тип топлива: {self.fuel_type}"
+
+    # Пример использования классов
+vehicle = Vehicle("Toyota", "Corolla")
+print(vehicle.get_info())
+car = Car("Honda", "Civic", "Бензин")
+print(car.get_info())
+
+'''class UserAccount:    
+    def __init__(self, username, email, password):        
+        self.username = username        self.email = email        
+        self.password = password  # Приватный атрибут   
+         
+    def set_password(self, new_password):              
+    self.password = new_password   
+     
+    def check_password(self, password):
+        return self.password == password
+        
+#Пример использования класса UserAccount
+            
+    # Создание объекта класса UserAccount
+user = UserAccount("anna", "rar.com", "000")    
+    # Изменение пароля
+user.set_password("111")
+    # Проверка пароля
+print(user.check_password("wrongPassword")) 
+print(user.check_password("456")) '''
+
